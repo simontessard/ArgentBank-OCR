63d43992e32779bd1c12c40c60099412b429e904
Simon Tessard <simon.tessard@gmail.com>
2023-03-31 00:54:32 +0200

    Add : feature component integration

---
+++ src/components/Feature.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+const FeatureItem = styled.div`
+  flex: 1;
+  padding: 2.5rem;
+`
+const FeatureIcon = styled.img`
+  width: 100px;
+  border: 10px solid #00bc77;
+  border-radius: 50%;
+  padding: 1rem;
+`
+const FeatureTitle = styled.h3`
+  color: #222;
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`
+
+function Feature({ image, alt, title, text }) {
+  return (
+    <FeatureItem>
+      <FeatureIcon src={image} alt={alt} />
+      <FeatureTitle>{title}</FeatureTitle>
+      <p>{text}</p>
+    </FeatureItem>
+  )
+}
+
+export default Feature
