78add765bbd63f2b56e548c8f577a2edd5075856
Simon Tessard <simon.tessard@gmail.com>
2023-03-31 20:04:46 +0200

    Transaction component integration

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+
+const AccountContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  background-color: #fff;
+  width: 80%;
+  margin: 0 auto;
+  flex-direction: column;
+  padding: 1.5rem;
+  box-sizing: border-box;
+  text-align: left;
+  margin-bottom: 1rem;
+  border-radius: 5px;
+  @media (min-width: 720px) {
+    flex-direction: row;
+  }
+`
+const AccountWrapper = styled.section`
+  width: 100%;
+  flex: 1;
+`
+
+const AccountWrapperCTA = styled(AccountWrapper)`
+  @media (min-width: 720px) {
+    flex: 0;
+  }
+`
+
+const AccountTitle = styled.h3`
+  margin: 0;
+  padding: 0;
+  font-size: 1rem;
+  font-weight: normal;
+`
+
+const AccountAmount = styled.p`
+  margin: 0;
+  font-size: 2.5rem;
+  font-weight: bold;
+`
+
+const AccountAmountDescription = styled.p`
+  margin: 0;
+`
+
+const TransactionButton = styled.button`
+  border-radius: 5px;
+
+  display: block;
+  width: 100%;
+  padding: 8px;
+  font-size: 1.1rem;
+  font-weight: bold;
+  margin-top: 1rem;
+  border-color: #00bc77;
+  background-color: #00bc77;
+  color: #fff;
+  @media (min-width: 720px) {
+    width: 200px;
+  }
+`
+
+function Transaction({ title, amount }) {
+  return (
+    <AccountContainer>
+      <AccountWrapper>
+        <AccountTitle>{title}</AccountTitle>
+        <AccountAmount>{amount}</AccountAmount>
+        <AccountAmountDescription>Available Balance</AccountAmountDescription>
+      </AccountWrapper>
+      <AccountWrapperCTA>
+        <TransactionButton>View transactions</TransactionButton>
+      </AccountWrapperCTA>
+    </AccountContainer>
+  )
+}
+
+export default Transaction
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,29 +1,13 @@
 import styled from 'styled-components'
 
+import Transaction from '../components/Transaction'
+
 const MainContainer = styled.main`
   background-color: #12002b;
   flex: 1;
   padding-block: 2rem;
 `
 
-const AccountContainer = styled.section`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid black;
-  background-color: #fff;
-  width: 80%;
-  margin: 0 auto;
-  flex-direction: column;
-  padding: 1.5rem;
-  box-sizing: border-box;
-  text-align: left;
-  margin-bottom: 1rem;
-  border-radius: 5px;
-  @media (min-width: 720px) {
-    flex-direction: row;
-  }
-`
 const AccountContainerTitle = styled.h2`
   border: 0 !important;
   clip: rect(1px, 1px, 1px, 1px) !important; /* 1 */
@@ -38,34 +22,6 @@ const AccountContainerTitle = styled.h2`
   white-space: nowrap !important; /* 3 */
 `
 
-const AccountWrapper = styled.section`
-  width: 100%;
-  flex: 1;
-`
-
-const AccountWrapperCTA = styled(AccountWrapper)`
-  @media (min-width: 720px) {
-    flex: 0;
-  }
-`
-
-const AccountTitle = styled.h3`
-  margin: 0;
-  padding: 0;
-  font-size: 1rem;
-  font-weight: normal;
-`
-
-const AccountAmount = styled.p`
-  margin: 0;
-  font-size: 2.5rem;
-  font-weight: bold;
-`
-
-const AccountAmountDescription = styled.p`
-  margin: 0;
-`
-
 const EditButton = styled.button`
   border-radius: 5px;
   border-color: #00bc77;
@@ -73,23 +29,6 @@ const EditButton = styled.button`
   color: #fff;
   font-weight: bold;
   padding: 10px;
-`
-
-const TransactionButton = styled.button`
-  border-radius: 5px;
-
-  display: block;
-  width: 100%;
-  padding: 8px;
-  font-size: 1.1rem;
-  font-weight: bold;
-  margin-top: 1rem;
-  border-color: #00bc77;
-  background-color: #00bc77;
-  color: #fff;
-  @media (min-width: 720px) {
-    width: 200px;
-  }
 `
 
 const Header = styled.div`
@@ -109,42 +48,9 @@ function Account() {
         <EditButton class="edit-button">Edit Name</EditButton>
       </Header>
       <AccountContainerTitle class="sr-only">Accounts</AccountContainerTitle>
-      <AccountContainer>
-        <AccountWrapper class="account-content-wrapper">
-          <AccountTitle class="account-title">Argent Bank Checking (x8349)</AccountTitle>
-          <AccountAmount class="account-amount">$2,082.79</AccountAmount>
-          <AccountAmountDescription class="account-amount-description">
-            Available Balance
-          </AccountAmountDescription>
-        </AccountWrapper>
-        <AccountWrapperCTA class="account-content-wrapper cta">
-          <TransactionButton class="transaction-button">View transactions</TransactionButton>
-        </AccountWrapperCTA>
-      </AccountContainer>
-      <AccountContainer>
-        <AccountWrapper class="account-content-wrapper">
-          <AccountTitle class="account-title">Argent Bank Savings (x6712)</AccountTitle>
-          <AccountAmount class="account-amount">$10,928.42</AccountAmount>
-          <AccountAmountDescription class="account-amount-description">
-            Available Balance
-          </AccountAmountDescription>
-        </AccountWrapper>
-        <AccountWrapperCTA class="account-content-wrapper cta">
-          <TransactionButton class="transaction-button">View transactions</TransactionButton>
-        </AccountWrapperCTA>
-      </AccountContainer>
-      <AccountContainer>
-        <AccountWrapper class="account-content-wrapper">
-          <AccountTitle class="account-title">Argent Bank Credit Card (x8349)</AccountTitle>
-          <AccountAmount class="account-amount">$184.30</AccountAmount>
-          <AccountAmountDescription class="account-amount-description">
-            Current Balance
-          </AccountAmountDescription>
-        </AccountWrapper>
-        <AccountWrapperCTA class="account-content-wrapper cta">
-          <TransactionButton class="transaction-button">View transactions</TransactionButton>
-        </AccountWrapperCTA>
-      </AccountContainer>
+      <Transaction title="Argent Bank Checking (x8349)" amount="$2,082.79" />
+      <Transaction title="Argent Bank Savings (x6712)" amount="$10,928.42" />
+      <Transaction title="Argent Bank Credit Card (x8349)" amount="$184.30" />
     </MainContainer>
   )
 }
