336a6dc7bf6e4e8412755983d2d7337e56c521f4
Simon Tessard <simon.tessard@gmail.com>
2023-04-02 18:14:28 +0200

    Making redux store available

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import Routing from './Router'
 import './index.css'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Routing />
+    <Provider store={store}>
+      <Routing />
+    </Provider>
   </React.StrictMode>
 )
