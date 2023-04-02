6afa5798091356a61e27895e31eb785a8e631745
Simon Tessard <simon.tessard@gmail.com>
2023-04-02 22:27:41 +0200

    Added token to state

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,8 +1,11 @@
 import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from '../actions/types'
 
 const user = JSON.parse(localStorage.getItem('user'))
+const token = localStorage.getItem('token')
 
-const initialState = user ? { isLoggedIn: true, user } : { isLoggedIn: false, user: null }
+const initialState = user
+  ? { isLoggedIn: true, user, token }
+  : { isLoggedIn: false, user: null, token: null }
 
 export default function authReducer(state = initialState, action) {
   const { type, payload } = action
@@ -13,6 +16,7 @@ export default function authReducer(state = initialState, action) {
         ...state,
         isLoggedIn: true,
         user: payload.user,
+        token: payload.user.body.token,
       }
     case LOGIN_FAIL:
       return {
