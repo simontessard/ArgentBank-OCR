e95ac25fa9b713c8a9a7f58e0436b216e6f5eb67
Simon Tessard <simon.tessard@gmail.com>
2023-04-02 22:28:40 +0200

    Improved handle of errors

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,10 +3,13 @@ import authHeader from './auth-header'
 
 const API_URL = 'http://localhost:3001/api/v1/user/'
 
-export const getUserProfile = () => {
-  return axios.post(API_URL + 'profile', { headers: authHeader() })
-}
-
-export const updateUserProfile = () => {
-  return axios.put(API_URL + 'profile', { headers: authHeader() })
+export const getUserProfile = (token) => {
+  return axios
+    .post(API_URL + 'profile', { headers: authHeader(token) })
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      throw error.response.data
+    })
 }
