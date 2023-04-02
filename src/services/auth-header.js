6bb8257429ec5edfa9b4b955c9b4576d6b01d9dd
Simon Tessard <simon.tessard@gmail.com>
2023-04-02 23:11:53 +0200

    Simplified code

--- src/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader(token) {
-  const user = JSON.parse(localStorage.getItem('user'))
-
-  if (user && user.accessToken) {
-    return { Authorization: 'Bearer ' + token }
-  } else {
-    return {}
-  }
-}
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
-import authHeader from './auth-header'
 
 const API_URL = 'http://localhost:3001/api/v1/user/'
 
 export const getUserProfile = (token) => {
   return axios
-    .post(API_URL + 'profile', { headers: authHeader(token) })
+    .post(API_URL + 'profile', { headers: { Authorization: 'Bearer ' + token } })
     .then((response) => {
       return response.data
     })
