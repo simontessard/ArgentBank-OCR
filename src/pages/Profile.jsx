b04aabf1870c62c22a84063cac15e2b24388328e
Simon Tessard <simon.tessard@gmail.com>
2023-04-02 18:57:19 +0200

    Removed class

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -27,7 +27,7 @@ function Profile() {
   return (
     <MainContainer>
       <Welcome firstName="Tony" lastName="Stark" />
-      <AccountContainerTitle class="sr-only">Accounts</AccountContainerTitle>
+      <AccountContainerTitle>Accounts</AccountContainerTitle>
       <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" />
       <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" />
       <Account title="Argent Bank Credit Card (x8349)" amount="$184.30" />
