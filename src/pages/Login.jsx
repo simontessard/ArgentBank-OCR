1aae20e8ad1ba5c42f81bbc042e20de4aaf9ad6d
Simon Tessard <simon.tessard@gmail.com>
2023-04-03 13:16:44 +0200

    Add: redirect to profile page when logged in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,20 @@ import styled from 'styled-components'
 
 import Form from '../components/Form'
 
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+
 const MainContainer = styled.main`
   background-color: #12002b;
   display: flex;
 `
 function Login() {
+  const { isLoggedIn } = useSelector((state) => state.auth)
+
+  if (isLoggedIn) {
+    return <Navigate to="/profile" />
+  }
+
   return (
     <MainContainer>
       <Form />
