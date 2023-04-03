5a3ffe630dfdbcb06ad61b02aabe41e573f64614
Simon Tessard <simon.tessard@gmail.com>
2023-04-03 22:59:44 +0200

    Edit profile style

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import React, { useState } from 'react'
+
 const EditButton = styled.button`
   border-radius: 5px;
   border-color: #00bc77;
@@ -9,22 +11,99 @@ const EditButton = styled.button`
   padding: 10px;
   cursor: pointer;
 `
-
-const Header = styled.div`
+const SubmitButton = styled.button`
+  display: block;
+  width: 100%;
+  padding: 8px;
+  font-size: 1.1rem;
+  font-weight: bold;
+  margin-top: 1rem;
+  border-color: #00bc77;
+  background-color: #00bc77;
   color: #fff;
+  cursor: pointer;
+  border-radius: 5px;
+`
+
+const CancelButton = styled(SubmitButton)`
+  background-color: red;
+  border-color: red;
+`
+
+const WelcomeMessage = styled.div`
   margin-bottom: 2rem;
+  color: #fff;
+`
+
+const EditContainer = styled.div`
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+const Input = styled.input`
+  border-radius: 5px;
+  padding: 8px;
+  margin-top: 0rem;
+`
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const ContainerButtons = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+const LabelCustom = styled.label`
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+  text-align: left;
 `
 
 function Welcome({ firstName, lastName }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedFirstName, setEditedFirstName] = useState(firstName)
+  const [editedLastName, setEditedLastName] = useState(lastName)
+
+  const handleSave = () => {
+    setIsEditing(false)
+  }
+
+  const handleCancel = () => {
+    setIsEditing(false)
+  }
+
   return (
-    <Header>
-      <h1>
-        Welcome back
-        <br />
-        {firstName} {lastName}!
-      </h1>
-      <EditButton>Edit Name</EditButton>
-    </Header>
+    <Container>
+      {isEditing ? (
+        <EditContainer>
+          <LabelCustom>First name</LabelCustom>
+          <Input
+            type="text"
+            value={firstName}
+            onChange={(e) => setEditedFirstName(e.target.value)}
+          />
+          <LabelCustom>Last name</LabelCustom>
+          <Input type="text" value={lastName} onChange={(e) => setEditedLastName(e.target.value)} />
+          <ContainerButtons>
+            <CancelButton onClick={handleCancel}>Cancel</CancelButton>
+            <SubmitButton onClick={handleSave}>Save</SubmitButton>
+          </ContainerButtons>
+        </EditContainer>
+      ) : (
+        <WelcomeMessage>
+          <h1>
+            Welcome back
+            <br />
+            {firstName} {lastName}!
+          </h1>
+          <EditButton onClick={() => setIsEditing(true)}>Edit Name</EditButton>
+        </WelcomeMessage>
+      )}
+    </Container>
   )
 }
 
