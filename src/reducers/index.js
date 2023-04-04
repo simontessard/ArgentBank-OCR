d2a2373771d7f2d52f2b35368ca90f2d0767ddfd
Simon Tessard <simon.tessard@gmail.com>
2023-04-04 11:48:50 +0200

    Add: comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,10 @@ import { combineReducers } from 'redux'
 import auth from './auth'
 import message from './message'
 
+/* The combineReducers function turns an object whose values are different reducing functions 
+into a single reducing function you can pass to createStore.
+The resulting reducer calls every child reducer, and gathers their results into a single state object. */
+
 export default combineReducers({
   auth,
   message,
